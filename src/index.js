53ab16c397f9d11b2186a2625cf7c946363e0305
matheus3070 <matheeus.estudos2002@gmail.com>
2026-02-09 17:52:34 -0300

    implementando estruturas condicionais

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-console.log("Olá");
+let nome = "Matheus";
+let nivel = 1000;
 
-console.log("Testando");
+if (nivel <= 1000){
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+} else if (nivel >= 1001 && nivel <= 2000){
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+}
